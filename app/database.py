cef8b561797a038ed6a5daaa3c82173866c29bbd
batumutsu <baptiste.mutuyeyezu@rssb.rw>
2024-09-01 19:39:15 +0200

    test mysql

--- app/database.py
+++ app/database.py
@@ -7,8 +7,9 @@ import os
 load_dotenv()
 
 #DATABASE_URL = "postgresql://quick_start:quick_start@localhost/inkomoko"
+DATABASE_URL = "mysql+pymysql://batumutsu:inkomoko@batumutsu.mysql.pythonanywhere-services.com/batumutsu$inkomoko"
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+# DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
