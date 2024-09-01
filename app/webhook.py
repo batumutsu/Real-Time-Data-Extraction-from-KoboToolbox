2d5d4dfe2e659bc640c4a758a2aa0abc9aadd69a
batumutsu <baptiste.mutuyeyezu@rssb.rw>
2024-09-01 19:17:40 +0200

    test

--- app/webhook.py
+++ app/webhook.py
@@ -13,37 +13,37 @@ app = Flask(__name__)
 
 @app.route('/api/webhook/real-time-updates', methods=['POST'])
 def webhook():
-    data = request.json
-    db = next(get_db())
+    return "Hello, how have you been?"
+    # data = request.json
+    # db = next(get_db())
     
-    processed_data = process_kobo_data({'results': [data]})
-    if processed_data:
-        record = processed_data[0]
-        kobo_id = record['kobo_id']
-        existing_record = db.query(KoboRecord).filter(KoboRecord.kobo_id == kobo_id).first()
+    # processed_data = process_kobo_data({'results': [data]})
+    # if processed_data:
+    #     record = processed_data[0]
+    #     kobo_id = record['kobo_id']
+    #     existing_record = db.query(KoboRecord).filter(KoboRecord.kobo_id == kobo_id).first()
         
-        if existing_record:
-            for key, value in record.items():
-                setattr(existing_record, key, value)
-        else:
-            new_record = KoboRecord(**record)
-            db.add(new_record)
+    #     if existing_record:
+    #         for key, value in record.items():
+    #             setattr(existing_record, key, value)
+    #     else:
+    #         new_record = KoboRecord(**record)
+    #         db.add(new_record)
         
-        db.commit()
-        return jsonify({"status": "success"}), 200
-    else:
-        return jsonify({"status": "error", "message": "Invalid data format"}), 400
+    #     db.commit()
+    #     return jsonify({"status": "success"}), 200
+    # else:
+    #     return jsonify({"status": "error", "message": "Invalid data format"}), 400
 
 def register_webhook():
     INKOMOKO_REGISTER_WEBHOOK_URL = os.getenv('INKOMOKO_REGISTER_WEBHOOK_URL')
     REAL_TIME_POST_ENDPOINT_URL = os.getenv('REAL_TIME_POST_ENDPOINT_URL')
     url = INKOMOKO_REGISTER_WEBHOOK_URL
-    return "Hello, how have you been?"
-    # payload = json.dumps({"url": REAL_TIME_POST_ENDPOINT_URL})
-    # headers = {'Content-Type': 'application/json'}
-    # response = requests.post(url, headers=headers, data=payload)
-    # if response.status_code == 200:
-    #     return response
-    # else:
-    #     raise Exception(f"Failed to register webhook: {response.text}")
+    payload = json.dumps({"url": REAL_TIME_POST_ENDPOINT_URL})
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, headers=headers, data=payload)
+    if response.status_code == 200:
+        return response
+    else:
+        raise Exception(f"Failed to register webhook: {response.text}")
     
