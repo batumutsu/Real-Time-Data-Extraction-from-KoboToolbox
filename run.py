8e39f092676a05c7519145874f5930a3d1dbfc17
batumutsu <baptiste.mutuyeyezu@rssb.rw>
2024-09-01 20:24:12 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app.database import init_db, engine
 from app.models import Base
 
 if __name__ == "__main__":
-    #Base.metadata.drop_all(bind=engine)  # This drops all tables
+    Base.metadata.drop_all(bind=engine)  # This drops all tables
     init_db()  # This creates all tables
     save_extracted_records()
     app.run(host='102.22.140.126', port=4000)
